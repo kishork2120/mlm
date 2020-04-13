554c992198bd3cafadd156c2280a15bbe9fac65a
kishork2120 <kishork2120@gmail.com>
2020-04-13 15:27:49 +0530

    env implemented

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.use(express.static('public'))
 
@@ -8,7 +9,7 @@ app.get('/',(req,res)=>{
     // res.sendFile(`${__dirname}/index.html`);
 })
 
-app.listen(3000,(err)=>{
+app.listen(process.env.PORT,(err)=>{
     if(err) console.log(err)
-    else console.log('Server on 3000 port')
+    else console.log(`Server on ${process.env.PORT} port`)
 })
