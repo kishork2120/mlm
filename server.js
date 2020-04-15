9b555a573f9dba839a60b888575d5ba851e1aa62
Kishor Kumar <kishor@cartrabbit.in>
2020-04-15 16:25:56 +0530

    changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const path = require('path');
 
-app.use(express.static('html/public'))
+app.use(express.static(path.join(__dirname,'html/public')))
 
 app.get('/',(req,res)=>{
     res.sendFile(`${__dirname}/html/coming_soon.html`);
